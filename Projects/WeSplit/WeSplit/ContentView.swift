b93b4a07ef8eaacb4185e330b467a7aa9ae558fa
SecondSerenity <gmaidenmueller@gmail.com>
2021-10-16 09:01:47 -0500

    Day 18: Project 1 Challenges

--- Projects/WeSplit/WeSplit/ContentView.swift
+++ Projects/WeSplit/WeSplit/ContentView.swift
@@ -8,21 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
-    @State private var tipPercentage = 2
+    @State private var calculateButtonShown = true
+    @State private var totalsShown = false
     
-    var totalPerPerson: String {
-        let check_amount = Double(checkAmount) ?? 0
-        let total_people = Double(self.numberOfPeople + 2)
-        
-        let tip_percent = Double(tipPercentages[tipPercentage]) / 100
-        let tip_total = check_amount * tip_percent
-        
-        let split_total = (check_amount + tip_total) / total_people
-        
-        return String(format: "%.2f", split_total)
-    }
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercentage = 2
     
     let tipPercentages = [10, 15, 20, 25, 0]
     			
@@ -32,12 +23,15 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)
-                    
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2 ..< 100) {
-                            Text("\($0) people")
+                        .onChange(of: checkAmount) { newValue in
+                            onTextFieldChange()
                         }
-                    }
+                    
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
+                        .onChange(of: numberOfPeople) { newValue in
+                            onTextFieldChange()
+                        }
                 }
                 
                 Section(header: Text("How much tip do you want to leave?")) {
@@ -49,14 +43,62 @@ struct ContentView: View {
                     .pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section(header: Text("Total Per Person")) {
-                    Text("$\(totalPerPerson)")
+                if calculateButtonShown {
+                    Button("Calculate") {
+                        if (checkAmount == "" || numberOfPeople == "") {
+                            return
+                        }
+                        
+                        hideKeyboard()
+                        calculateButtonShown = false
+                        totalsShown = true
+                    }
+                }
+                
+                if totalsShown {
+                    let total_bill = calculateAmountPlusTip()
+                    let total_per_person = total_bill / sanitizeTotalPeople()
+                    
+                    Section(header: Text("Amount + Tip")) {
+                        Text("$\(total_bill, specifier: "%.2f")")
+                    }
+                    Section(header: Text("Amount Per Person")) {
+                        Text("$\(total_per_person, specifier: "%.2f")")
+                    }
                 }
             }
             .navigationTitle("WeSplit")
         }
     }
+    
+    func onTextFieldChange() {
+        self.calculateButtonShown = true
+        self.totalsShown = false
+    }
+    
+    func sanitizeTotalPeople() -> Double {
+        // Convert the string to an int first if they attached another keyboard and typed a floating point
+        let converted_people = Int(numberOfPeople) ?? 0
+        return Double(converted_people)
+    }
+    
+    func calculateAmountPlusTip() -> Double {
+        let check_amount = Double(checkAmount) ?? 0
+        
+        let tip_percent = Double(tipPercentages[tipPercentage]) / 100
+        let tip_total = check_amount * tip_percent
+        
+        return check_amount + tip_total
+    }
 }
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

5ac02c09ca97c463c61e00430212e3f9499ba58e
SecondSerenity <gmaidenmueller@gmail.com>
2021-11-06 21:55:09 -0500

    Day 39: Scroll views, fitting images, navigation views, JSON decode

---
+++ Notes/Day 39.swift
@@ -0,0 +1,92 @@
+import SwiftUI
+import PlaygroundSupport
+
+struct AutoFitImages: View {
+    var body: some View {
+        VStack {
+            GeometryReader { geo in
+                Image("Example")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: geo.size.width)
+            }
+        }
+    }
+}
+
+struct CustomText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a CustomText: \(text)")
+        self.text = text
+    }
+}
+
+struct ScrollViewDemo: View {
+    var body: some View {
+        ScrollView(.vertical) {
+            // Will instantiate all children immediately
+            // List will only instantiate the ones in view
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+struct NavigateToViews: View {
+    var body: some View {
+        NavigationView {
+            List(0..<100) { row in
+                NavigationLink(destination: Text("Detail \(row)")) {
+                    Text("Row \(row)")
+                }
+            }
+            .navigationBarTitle("Swift UI")
+        }
+    }
+}
+
+struct DecodeJsonIntoObject: View {
+    var body: some View {
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
+            }
+            """
+            
+            struct User: Codable {
+                var name: String
+                var address: Address
+            }
+            
+            struct Address: Codable {
+                var street: String
+                var city: String
+            }
+            
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
+        }
+    }
+}
+
+PlaygroundPage.current.setLiveView(DecodeJsonIntoObject())
---
+++ Projects/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Geoffrie Maiden Mueller on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

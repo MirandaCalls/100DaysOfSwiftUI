54911ebd33b1de0f9a17253226e06f0d3b311fd8
SecondSerenity <gmaidenmueller@gmail.com>
2021-11-11 21:44:17 -0600

    Day 44: More complicated drawing and Metal

--- Projects/Drawing/Drawing/ContentView.swift
+++ Projects/Drawing/Drawing/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack() {
-            // TriangleRawPath()
-            // TriangleShape()
-            // InsettableArc()
-            PercentageProgress()
-            Spacer()
-        }
+        // TriangleRawPath()
+        // TriangleShape()
+        // InsettableArc()
+        // PercentageProgress()
+        // Spacer()
+        // GroovyPetals()
+        // MeowBorder()
+        MetalDrawingGroup()
     }
 }
 
@@ -94,6 +95,97 @@ struct PercentageProgress: View {
                 .bold()
         }
         .frame(width: 300, height: 300)
+    }
+}
+
+struct Flower: Shape {
+    var petalOffset: Double = -20
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+        }
+        
+        return path
+    }
+}
+
+struct GroovyPetals: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
+    var body: some View {
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(Color.red, style: FillStyle(eoFill: true))
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        }
+    }
+}
+
+struct MeowBorder: View {
+    var body: some View {
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("Cat"), scale: 0.08), lineWidth: 20)
+            .frame(width: 300, height: 200)
+    }
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct MetalDrawingGroup: View {
+    @State private var colorCycle = 0.0
+    
+    var body: some View {
+        VStack {
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+            
+            Slider(value: $colorCycle)
+        }
     }
 }
 

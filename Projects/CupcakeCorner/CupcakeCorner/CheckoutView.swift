f8d0067414c7ee72df01a2935a64ee5fa2246e6c
SecondSerenity <gmaidenmueller@gmail.com>
2021-11-18 20:13:48 -0600

    Day 51: CupcakeCorner now submits orders to a web service

--- Projects/CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ Projects/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         GeometryReader { geo in
             ScrollView(.vertical) {
@@ -22,13 +25,43 @@ struct CheckoutView: View {
                     Text("Your total is $\(self.order.cost, specifier: "%.2f")")
                     
                     Button("Place order") {
-                        // place the order
+                        self.placeOrder()
                     }
                     .padding()
                 }
             }
         }
         .navigationBarTitle("Check Out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
+    }
+    
+    func placeOrder() {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            
+            if let decoded = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decoded.quantity) \(Order.types[decoded.type].lowercased()) cupcakes is on its way."
+                self.showingConfirmation = true
+            } else {
+                print("Invalid response from server")
+            }
+        }.resume()
     }
 }
 

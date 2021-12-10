7ef667247953cd7e4166e87e359fb59e0f4b30af
SecondSerenity <gmaidenmueller@gmail.com>
2021-12-09 21:44:09 -0600

    Day 72: Saving location pins to disk and Face ID auth

--- Projects/BucketList/BucketList/ContentView.swift
+++ Projects/BucketList/BucketList/ContentView.swift
@@ -7,44 +7,83 @@
 
 import SwiftUI
 import MapKit
+import LocalAuthentication
 
 struct ContentView: View {
     @State private var centerCoordinate = CLLocationCoordinate2D()
     @State private var selectedPlace: MKPointAnnotation?
     @State private var showingPlaceDetails = false
-    @State private var locations = [MKPointAnnotation]()
+    @State private var locations = [CodableMKPointAnnotation]()
     @State private var showingEditScreen = false
+    @State private var isUnlocked = false
     
     var body: some View {
         ZStack {
-            MapView(centerCoordinate: self.$centerCoordinate, selectedPlace: self.$selectedPlace, showingPlaceDetails: self.$showingPlaceDetails, annotations: locations)
-                .edgesIgnoringSafeArea(.all)
-            Circle()
-                .fill(Color.blue)
-                .opacity(0.3)
-                .frame(width: 32, height: 32)
-            VStack {
-                Spacer()
-                HStack {
+            if self.isUnlocked {
+                MapView(centerCoordinate: self.$centerCoordinate, selectedPlace: self.$selectedPlace, showingPlaceDetails: self.$showingPlaceDetails, annotations: locations)
+                    .edgesIgnoringSafeArea(.all)
+                Circle()
+                    .fill(Color.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
+                VStack {
                     Spacer()
-                    Button(action: {
-                        let new_location = MKPointAnnotation()
-                        new_location.title = "Example Location"
-                        new_location.coordinate = self.centerCoordinate
-                        self.locations.append(new_location)
-                        
-                        self.selectedPlace = new_location
-                        self.showingEditScreen = true
-                    }) {
-                        Image(systemName: "plus")
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            let new_location = CodableMKPointAnnotation()
+                            new_location.title = "Example Location"
+                            new_location.coordinate = self.centerCoordinate
+                            self.locations.append(new_location)
+                            
+                            self.selectedPlace = new_location
+                            self.showingEditScreen = true
+                        }) {
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(Color.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
                     }
-                    .padding()
-                    .background(Color.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
                 }
+            } else {
+                LinearGradient(colors: [.blue, .green], startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Spacer()
+                    
+                    VStack {
+                        HStack {
+                            Text("Bucket List")
+                                .font(.largeTitle)
+                            Image(systemName: "lock")
+                                .font(.largeTitle)
+                        }
+                        Text("Securely storing your plans for the future.")
+                            .padding(.top)
+                    }
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black.opacity(0.5))
+                    .cornerRadius(10)
+                    
+                    Spacer()
+                    
+                    Button("Unlock Data") {
+                        self.authenticate()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+                    .padding([.leading, .trailing])
+                }
+
             }
         }
         .alert(isPresented: self.$showingPlaceDetails) {
@@ -53,10 +92,58 @@ struct ContentView: View {
                 }
             )
         }
-        .sheet(isPresented: self.$showingEditScreen) {
+        .sheet(isPresented: self.$showingEditScreen, onDismiss: self.saveData) {
             if self.selectedPlace != nil {
                 EditView(placemark: self.selectedPlace!)
             }
+        }
+        .onAppear(perform: self.loadData)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Please authenticate to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        // error
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func loadData() {
+        let filename = self.getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+        
+        do {
+            let data = try Data(contentsOf: filename)
+            self.locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
+        } catch {
+            print("Unable to load saved places")
+        }
+    }
+    
+    func saveData() {
+        do {
+            let filename = self.getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+            let data = try JSONEncoder().encode(self.locations)
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+        } catch {
+            print("Unable to save data")
         }
     }
 }
---
+++ Projects/BucketList/BucketList/MKPointAnnotation-Codable.swift
@@ -0,0 +1,39 @@
+//
+//  MKPointAnnotation-Codable.swift
+//  BucketList
+//
+//  Created by Geoffrie Maiden Mueller on 12/9/21.
+//
+
+import Foundation
+import MapKit
+
+class CodableMKPointAnnotation: MKPointAnnotation, Codable {
+    enum CodingKeys: CodingKey {
+        case title, subtitle, latitude, longitude
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    required init(from decoder: Decoder) throws {
+        super.init()
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.subtitle = try container.decode(String.self, forKey: .subtitle)
+        
+        let latitude = try container.decode(CLLocationDegrees.self, forKey: .latitude)
+        let longitude = try container.decode(CLLocationDegrees.self, forKey: .longitude)
+        self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.title, forKey: .title)
+        try container.encode(self.subtitle, forKey: .subtitle)
+        try container.encode(self.coordinate.latitude, forKey: .latitude)
+        try container.encode(self.coordinate.longitude, forKey: .longitude)
+    }
+}
